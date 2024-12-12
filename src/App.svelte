c61464ace095d1af2d18d671789f4fa2a53b0765
Bartek Bak <bartek.bak@protonmail.com>
2024-12-12 01:04:56 +0000

    Added padding to the top of the page

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 <main class="w-full h-full grid place-content-center">
   {#if phonesVisible}
-    <div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-4 mt-20">
       <PhoneCard name="Mama" number="+44 7941 430438" pfpcolor="#EEF8FF" />
       <PhoneCard name="Tata" number="+44 7460 223422" pfpcolor="#EDF3FF" />
       <PhoneCard name="Bartek" number="+44 7419 7405787" pfpcolor="#F9F5FE" />
@@ -20,8 +20,12 @@
 
   <div
     on:click={() => {
-      phonesVisible = true;
+      if (phonesVisible === false) {
+        phonesVisible = true;
+      } else {
+        phonesVisible = false;
+      }
     }}
-    class="absolute bottom-0 right-0 z-3 w-24 h-24"
+    class="absolute bottom-0 right-0 z-3 w-14 h-36 translate-y-16"
   ></div>
 </main>
