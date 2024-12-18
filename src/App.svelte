e907a5894c4d85c8f32499662bf7bcca17c79117
Bartek Bak <bartek.bak@protonmail.com>
2024-12-18 20:51:02 +0000

    Fixed scrolling and incorrect margin when phones visible

--- src/App.svelte
+++ src/App.svelte
@@ -5,27 +5,27 @@
   let phonesVisible = false;
 </script>
 
-<main class="w-full h-full grid place-content-center">
+<main class="w-full h-full overflow-hidden">
   {#if phonesVisible}
-    <div class="flex flex-col gap-4 mt-20">
-      <PhoneCard name="Mama" number="+44 7941 430438" pfpcolor="#EEF8FF" />
-      <PhoneCard name="Tata" number="+44 7460 223422" pfpcolor="#EDF3FF" />
-      <PhoneCard name="Bartek" number="+44 7419 7405787" pfpcolor="#F9F5FE" />
-      <PhoneCard name="Piotrek" number="+44 7421 800644" pfpcolor="#ECFBF4" />
-      <PhoneCard name="Aga" number="+44 7869 376262" pfpcolor="#FDF2FB" />
+    <div class="h-full overflow-auto py-16">
+      <div class="flex flex-col gap-4 mx-10">
+        <PhoneCard name="Mama" number="+44 7941 430438" pfpcolor="#EEF8FF" />
+        <PhoneCard name="Tata" number="+44 7460 223422" pfpcolor="#EDF3FF" />
+        <PhoneCard name="Bartek" number="+44 7419 7405787" pfpcolor="#F9F5FE" />
+        <PhoneCard name="Piotrek" number="+44 7421 800644" pfpcolor="#ECFBF4" />
+        <PhoneCard name="Aga" number="+44 7869 376262" pfpcolor="#FDF2FB" />
+      </div>
     </div>
   {:else}
-    <LoginCard />
+    <div class="grid place-content-center h-full">
+      <LoginCard />
+    </div>
   {/if}
 
   <div
     on:click={() => {
-      if (phonesVisible === false) {
-        phonesVisible = true;
-      } else {
-        phonesVisible = false;
-      }
+      phonesVisible = !phonesVisible;
     }}
-    class="absolute bottom-0 right-0 z-3 w-14 h-36 translate-y-16"
-  ></div>
+    class="absolute bottom-0 right-0 z-3 w-14 h-36 bg-red-300"
+  />
 </main>
