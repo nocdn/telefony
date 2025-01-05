4aba9c75a985b3eee40c4f2cbcf9394fb4a044ad
Bartek Bak <99417935+nocdn@users.noreply.github.com>
2025-01-05 14:27:46 +0000

    a11y: input aria labels

--- src/components/LoginCard.svelte
+++ src/components/LoginCard.svelte
@@ -10,6 +10,7 @@
       class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
       id="email"
       placeholder="email@example.com"
+      aria-label="Email"
     />
   </div>
   <div id="password-field-continer" class="grid gap-2">
@@ -18,6 +19,7 @@
       type="password"
       class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
       id="password"
+      aria-label="Password"
     />
   </div>
   <button
